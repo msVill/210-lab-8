5e22fe695c64ba9a9ac23034c79e282c6b9c5e08
Martha Villalta <msv.ltmc@gmail.com>
2025-08-26 20:29:49 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Include input/output library
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
+// That was so difficult for me to understand (the setting up process took longer than expected)
+// I hope I can get used to it soon
